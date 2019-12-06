4c53eb189d55c10ea9a9e175d9d560bcba7c91d5
workkk <15990127563@163.com>
2019-12-06 14:26:32 +0800

    webpack学习day2

--- webpack/start/src/index.js
+++ webpack/start/src/index.js
@@ -1,19 +1,29 @@
 const _  = require('lodash')
-import './style.css'
-import star from './star.jpg'
+import print from './print'
+// import './style.css'
+// import star from './star.jpg'
+// import data from './data.xml'
 
 function component() {
   let element = document.createElement('div');
 
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello'); //css api
+  // element.classList.add('hello'); //css api
 
   // 把图像放入到div中
-  let image = new Image(400,300)
-  image.src = star
+  // let image = new Image(400,300)
+  // image.src = star
+  // element.appendChild(image)
 
-  element.appendChild(image)
+  // xml
+  // console.log(data)
+
+  element.innerHTML = "click me"
+  element.onclick = function (params) {
+    print()
+  }
+
 
   return element;
 }
--- webpack/start/webpack.config.js
+++ webpack/start/webpack.config.js
@@ -1,32 +1,22 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',  // 入口
+  mode: 'development',
+  devtool: 'inline-source-map', //source-map妙用，映射错误到源文件
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },  // 入口
   output: { // 出口
     path: path.resolve(__dirname, 'dist'), // 输出路径
-    filename: 'bundle.js' // 输出文件名
+    filename: '[name].bundle.js' // 输出文件名
   },
-  module: { //loader
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.woff/,
-        use: [
-          'file-loader'
-        ]
-      }
-    ]
-  }
+  plugins:[
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: '管理输出'
+    })
+  ]
 }
