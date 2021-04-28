8f730413312781085797e3e527e646a2d6be9a13
workkk <gentlebob1998@icloud.com>
2021-04-28 22:56:54 +0800

    ts: unkown'未知', 比any'小'一些的类型，更加语义话

---
+++ TypeScript/types/unknown.ts
@@ -0,0 +1,66 @@
+// unknown 也是一种顶级类型(全局超级类型/universal supertype)。
+
+
+let value: unknown;
+
+value = true;             // OK
+value = 42;               // OK
+value = "Hello World";    // OK
+value = [];               // OK
+value = {};               // OK
+value = Math.random;      // OK
+value = null;             // OK
+value = undefined;        // OK
+value = new TypeError();  // OK
+value = Symbol("type");   // OK
+
+// 赋值
+let value1: unknown = value;   // OK
+let value2: any = value;       // OK
+let value3: boolean = value;   // Error
+let value4: number = value;    // Error
+let value5: string = value;    // Error
+let value6: object = value;    // Error
+let value7: any[] = value;     // Error
+let value8: Function = value;  // Error
+
+// 联合类型 因为这个就是取最大范围，如果是unknown和any那就是any
+type foo = any | unknown;
+
+// 交叉类型，取最小范围
+type bar = unknown & string;
+
+type Result =
+  | { success: true, value: unknown }
+  | { success: false, error: Error };
+
+// 这是一个例子，假设有个不知道类型的参数，就可以使用unknown
+function tryDeserializeLocalStorageItem(key: string): Result {
+  const item = localStorage.getItem(key);
+
+  if (item === null) {
+    // The item does not exist, thus return an error result
+    return {
+      success: false,
+      error: new Error(`Item with key "${key}" does not exist`)
+    };
+  }
+
+  let value: unknown;
+
+  try {
+    value = JSON.parse(item);
+  } catch (error) {
+    // The item is not valid JSON, thus return an error result
+    return {
+      success: false,
+      error
+    };
+  }
+
+  // Everything's fine, thus return a success result
+  return {
+    success: true,
+    value
+  };
+}
