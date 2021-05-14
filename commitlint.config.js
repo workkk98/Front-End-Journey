59b77a221641f48f8ad92ee844ae12c150015ada
workkk <gentlebob1998@icloud.com>
2021-05-14 22:18:36 +0800

    regexp: 正则的三种实现方式BRE, ERE, PCRE

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    "type-enum": [2, 'always', [
+    "type-enum": [1, 'always', [
       'ts',
       'js',
       'css',
